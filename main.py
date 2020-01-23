6f795fadabf0b79fed06c67d40ef9f52e2f7450c
brojass <brojas61@gmail.com>
2020-01-23 17:41:36 -0300

    first working version with comments and a new def read_configuration_list

--- main.py
+++ main.py
@@ -15,15 +15,17 @@ PATTERN_HOST = r'\[\s*host'
 PATTERN_ROOT = r'\[\s*root_folder'
 PATTERN_FOLDER = r'\[\s*folder'
 FILE_CONF = 'gea.config'
-HASH_FILE = 'hash'
+FILE_CONF_LIST = ['gea.config']
+REFERENCE_FILE = 'hash'
 
 
 def append_delimiter(directory):
     """
-
-    :param directory:
+    This function is responsible to skip specials characters '.' and './' from the path.
+    Otherwise add '/' to the path.
+    :param directory: The name or direction path from the pattern
     :type directory: str
-    :return:
+    :return: The path with '/' at the final of each one
     :rtype: str
     """
     if directory in ['.', './']:
@@ -35,10 +37,10 @@ def append_delimiter(directory):
 
 def return_key(line):
     """
-
-    :param line:
+    Function that separate the name or direction path (xxx) from the pattern ([host/root_folder/folder=xxx])
+    :param line: Line of text that contain '[host/root_folder/folder=xxx]' format.
     :type line: str
-    :return:
+    :return: The name or direction path from the pattern
     :rtype: str
     """
     aux = line.replace('=', ' ').replace('[', ' ').replace(']', ' ').split()
@@ -49,22 +51,32 @@ def return_key(line):
         raise ValueError('Error, key not found inside [] in ' + FILE_CONF)
 
 
-def read_configuration(file_name):
+def read_configuration_list(conf_list):
     """
 
-    :param file_name:
-    :type file_name: str
+    :param conf_list:
+    :type conf_list: list
     :return:
+    :rtype:
+    """
+    for config in conf_list:
+        for config_files_list in read_configuration(config):
+            print(config_files_list)
+
+
+def read_configuration(file_name):
+    """
+    This function read the file configuration and have an state machine that separate
+    the host and root folder from the folders who has the files to analyze, and as result
+    have all files found inside the configuration file.
+    :param file_name: The name of the path file who has the configuration.
+    :type file_name: str
+    :return: A list with files found inside the configuration file, but not the total of them.
     :rtype: list
     """
     root_folder = ''
     folder = ''
-    # output_list = []
-    # try:
     f = open(file_name, 'r')
-    # except IOError:
-    #     print('Error')
-    #     return False
     state = STATE_START
     for line in f:
         line = line.strip()
@@ -72,6 +84,7 @@ def read_configuration(file_name):
             continue
         if re.search(r'^#', line):
             continue
+
         if state == STATE_START:
             if re.search(PATTERN_HOST, line):
                 host = return_key(line)
@@ -110,15 +123,16 @@ def read_configuration(file_name):
                     raise ValueError('folder not defined')
             else:
                 file_list.append(root_folder + folder + line.strip())
+    f.close()
     return file_list
 
 
 def expand(full_file_name):
     """
-
-    :param full_file_name:
+    Function that find and transform all files which are in '*.xxx' format to path format.
+    :param full_file_name: file that were found inside the configuration file.
     :type full_file_name: str
-    :return:
+    :return: file in path format.
     :rtype: list
     """
     val = full_file_name.rfind('/')
@@ -130,27 +144,25 @@ def expand(full_file_name):
 
 def build_expanded_list(file_name_list):
     """
-
-    :param file_name_list:
+    This function iterates over the list who has the files that were found in first instance, for later find the files
+    that are in '*.xxx' format.
+    :param file_name_list: list with files that were found inside the configuration file, but not the total of them.
     :type file_name_list: list
-    :return:
+    :return: list with total files inside the configuration file.
     :rtype: list
     """
     output_list = []
     for file_name in file_name_list:
         for expanded_file_name in expand(file_name):
-            print(expanded_file_name)
             output_list.append(str(expanded_file_name))
     return output_list
 
 
 def print_list(input_list):
     """
-
-    :param input_list:
+    Function that iterates over a list and print each element.
+    :param input_list: list which wanna print.
     :type input_list: list
-    :return:
-    :rtype:
     """
     for element in input_list:
         print(element)
@@ -158,10 +170,10 @@ def print_list(input_list):
 
 def calculate_md5(file_name):
     """
-
-    :param file_name:
+    This function calculate md5 of each given file.
+    :param file_name: file as element that iterates on 'for' statement in def dictionary_hash.
     :type file_name: str
-    :return:
+    :return: md5 hash of given file.
     :rtype: str
     """
     f = open(file_name)
@@ -173,10 +185,11 @@ def calculate_md5(file_name):
 
 def dictionary_hash(files_list):
     """
-
-    :param files_list:
+    Function that iterates over list with the total files, for later calculate md5 of each file
+    to assign them into a dictionary.
+    :param files_list: list with total files inside the configuration file.
     :type files_list: list
-    :return:
+    :return: dictionary with md5 hash like value and as key, the files given.
     :rtype: dict
     """
     hash_dict = {}
@@ -188,10 +201,10 @@ def dictionary_hash(files_list):
 
 def hash_file_exist(file_hash):
     """
-
-    :param file_hash:
+    Function that read the content of REFERENCE_FILE.
+    :param file_hash: name or direction of REFERENCE_FILE.
     :type file_hash: str
-    :return:
+    :return: content of REFERENCE_FILE.
     :rtype: str
     """
     with open(file_hash, 'r') as f:
@@ -201,12 +214,13 @@ def hash_file_exist(file_hash):
 
 def compare_hash(reference_file_string_content, configuration_dictionary):
     """
-
-    :param reference_file_string_content:
+    This function compare the files between REFERENCE_FILE and configuration file. With the purpose
+    of known which files was modified, removed or added.
+    :param reference_file_string_content: content of REFERENCE_FILE.
     :type reference_file_string_content: str
-    :param configuration_dictionary:
+    :param configuration_dictionary: dictionary with md5 hash like value and files as key.
     :type configuration_dictionary: dict
-    :return:
+    :return: three list of modified files, removed files and added files.
     :rtype: tuple
     """
     removed_list = []
@@ -232,15 +246,13 @@ def compare_hash(reference_file_string_content, configuration_dictionary):
 
 def send_email(mod_files, rm_files, add_files):
     """
-
-    :param add_files:
+    Function that send the email of the changes that were detected to a specifics persons.
+    :param add_files: list that contain added files.
     :type add_files: list
-    :param mod_files:
+    :param mod_files: list that contain modified files.
     :type mod_files: list
-    :param rm_files:
+    :param rm_files: list that contain removed files.
     :type rm_files: list
-    :return:
-    :rtype:
     """
     aux_rm = ''
     aux_mod = ''
@@ -271,13 +283,11 @@ def send_email(mod_files, rm_files, add_files):
 
 def write_file(dictionary):
     """
-
-    :param dictionary:
+    Function that write or overwrite the REFERENCE_FILE
+    :param dictionary: dictionary with md5 hash like value and files as key.
     :type dictionary: dict
-    :return:
-    :rtype:
     """
-    with open('hash', 'w') as file:
+    with open(REFERENCE_FILE, 'w') as file:
         file.write(json.dumps(dictionary))
 
 
@@ -304,10 +314,10 @@ if __name__ == '__main__':
         exit(0)
     # print_list(expanded_file_list)
     dict_hash = dictionary_hash(expanded_file_list)
-    if os.path.exists(HASH_FILE):
-        reference_content = hash_file_exist(HASH_FILE)
+    if os.path.exists(REFERENCE_FILE):
+        reference_content = hash_file_exist(REFERENCE_FILE)
         modified_files, removed_files, added_files = compare_hash(reference_content, dict_hash)
         if modified_files or removed_files or added_files:
             print('Email send')
-            send_email(modified_files, removed_files, added_files)
+            # send_email(modified_files, removed_files, added_files)
     # write_file(dict_hash)
